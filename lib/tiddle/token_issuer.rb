3c26d076bae0a4533faf120f5459ee34caf404db
Oluwatosin Olubiyi <29473996+querldox5@users.noreply.github.com>
2020-09-11 00:58:05 +0100

    Update token_issuer.rb

--- lib/tiddle/token_issuer.rb
+++ lib/tiddle/token_issuer.rb
@@ -52,7 +52,7 @@ module Tiddle
       elsif resource.respond_to?(:relations) # Mongoid
         resource.relations['authentication_tokens'].klass
       elsif resource.respond_to?(:associations) # Dynamoid
-        resource..associations[:authentication_tokens]
+        resource.associations[:authentication_tokens]
       else
         raise 'Cannot determine authentication token class, unsupported ORM/ODM?'
       end
