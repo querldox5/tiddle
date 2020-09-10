990571d175981b1c62d33161ce1ec7ee6ac5f552
Oluwatosin Olubiyi <29473996+querldox5@users.noreply.github.com>
2020-09-10 23:56:11 +0100

    Update authentication_token.rb

--- spec/rails_app_dynamoiid/app/models/authentication_token.rb
+++ spec/rails_app_dynamoiid/app/models/authentication_token.rb
@@ -4,7 +4,6 @@ class AuthenticationToken
     belongs_to :user
 
     # specifying table name for this model, it'll be created if it does not exist yet
-    table name: :token_devices, key: :id, read_capacity: 5, write_capacity: 5
     field :id, :string
     field :user_id, :string
     field :body, :string, null: false
